96daee74dd67f0822ae0ea200490446f6f5b2f77
Anita Bieber <abieber1111gmail.com>
2023-08-11 09:20:09 +0200

    add Bracket

--- src/app/proposals/proposals.component.ts
+++ src/app/proposals/proposals.component.ts
@@ -8,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class ProposalsComponent implements OnInit {
   names = ['Michelle', 'Matthias', 'Hola'];
   texts = ['Sööön NY-View', 'Höhlenforscher', 'Jej!'];
-  images = ['assets/img/friends/1.jpg','assets/img/friends/2.jpg','assets/img/olive/8.jpg'];
+  images = ['assets/img/friends/1.jpg', 'assets/img/friends/2.jpg', 'assets/img/olive/8.jpg'];
   constructor() { }
-    ngOnInit(): void {
+  ngOnInit(): void {
     
   }
 
+}
